d54f36e17b18b487e942d95093371052cb157b74
Elin-powS <sopnil493@gmail.com>
2023-09-28 04:12:43 +0600

    Camera Galery and basic OCR Implementation

--- app/src/main/java/com/example/eduscan_pro/MainActivity.java
+++ app/src/main/java/com/example/eduscan_pro/MainActivity.java
@@ -2,26 +2,44 @@ package com.example.eduscan_pro;
 
 import static android.widget.Toast.makeText;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.Toast;
-
+import com.github.dhaval2404.imagepicker.ImagePicker;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.text.Text;
+import com.google.mlkit.vision.text.TextRecognition;
+import com.google.mlkit.vision.text.TextRecognizer;
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
+
+import java.io.IOException;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,6 +49,10 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     ImageView imageMenu;
 
+    FloatingActionButton camera,erase,copy;
+    EditText recogText;
+    Uri imageUri;
+    TextRecognizer textRecognizer;
     private FirebaseAuth firebaseAuth;
 
     @Override
@@ -47,6 +69,61 @@ public class MainActivity extends AppCompatActivity {
         imageMenu = findViewById(R.id.imageMenu);
         firebaseAuth = FirebaseAuth.getInstance();
         FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        //camera part
+
+        erase = findViewById(R.id.Erase);
+        camera = findViewById(R.id.Camera);
+        copy = findViewById(R.id.Copy);
+        recogText=findViewById(R.id.showdata);
+
+        copy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = recogText.getText().toString();
+
+                if(text.isEmpty()){
+                    Toast.makeText(MainActivity.this,"There is no Text to Copy!",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    ClipboardManager clipboardManager = (ClipboardManager) getSystemService(MainActivity.this.CLIPBOARD_SERVICE);
+                    ClipData clipData = ClipData.newPlainText("Data",recogText.getText().toString());
+                    clipboardManager.setPrimaryClip(clipData);
+
+                    Toast.makeText(MainActivity.this,"Text Copy to Clipborad",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        erase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = recogText.getText().toString();
+
+                if(text.isEmpty()){
+                    Toast.makeText(MainActivity.this,"There is no Text to Copy!",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    recogText.setText("-");
+                }
+            }
+        });
+
+        textRecognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
+
+
+
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImagePicker.Companion.with(MainActivity.this)
+                        .crop()	    			//Crop image(Optional), Check Customization for more option
+                        .compress(1024)			//Final image size will be less than 1 MB(Optional)
+                        .maxResultSize(1080, 1080)	//Final image resolution will be less than 1080 x 1080(Optional)
+                        .start();
+
+            }
+        });
 
         if (user == null) {
             startActivity(new Intent(MainActivity.this,Log_In.class));
@@ -98,6 +175,50 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+
+        if(resultCode == Activity.RESULT_OK){
+            if(data!=null){
+                imageUri = data.getData();
+
+                Toast.makeText(this,"image is Selected",Toast.LENGTH_SHORT).show();
+
+                recognizeText();
+            }
+        }
+        else {
+            Toast.makeText(this,"image is not Selected",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void recognizeText() {
+        if(imageUri!=null){
+            try {
+                InputImage inputImage = InputImage.fromFilePath(MainActivity.this,imageUri);
+
+                Task<Text> result = textRecognizer.process(inputImage)
+                        .addOnSuccessListener(new OnSuccessListener<Text>() {
+                            @Override
+                            public void onSuccess(Text text) {
+
+                                String recognizerText= text.getText();
+                                recogText.setText(recognizerText);
+
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(MainActivity.this, e.getMessage(),Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
     }
 }
 
